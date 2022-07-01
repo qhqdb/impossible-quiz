11561545f97ca98f4fa5c969a923858dbc724438
Michael Ha <mic.ha.el@me.com>
2022-06-30 17:07:40 -0700

    updated quiz

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# impossible-quiz
+# Impossible Quiz
+
+## Deployed Site
+ 
+(https://west-korea.github.io/impossible-quiz/)
+
+## User Story
+
+AS A coding boot camp student
+
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+
+SO THAT I can gauge my progress compared to my peers
+
+## Acceptance Criteria
+
+GIVEN I am taking a code quiz
+
+WHEN I click the start button
+
+THEN a timer starts and I am presented with a question
+
+WHEN I answer a question
+
+THEN I am presented with another question
+
+WHEN I answer a question incorrectly
+
+THEN time is subtracted from the clock
+
+WHEN all questions are answered or the timer reaches 0
+
+THEN the game is over
+
+WHEN the game is over
+
+THEN I can save my initials and score
+
+# Acknowledgments
+
+Inspiration, code snippets, etc.
+* [DomPizzie](https://gist.github.com/DomPizzie/7a5ff55ffa9081f2de27c315f5018afc#file-readme-template-md)
---
+++ assets/js/quiz.js
@@ -0,0 +1,92 @@
+var quizData = new Map([
+    [
+        "Who created JavaScript?",
+        ["Joe", "Henry Weigand", "Lindsay Fitzgerald", "William Lucht"]
+    ],
+    [
+        "What is the result of string(\"Quotes\")===\"Quotes\"?",
+        ["False", "False", "False", "SyntaxError"]
+    ],
+    [
+        "What is the correct JavaScript syntax to print \“Go Huskies\” in the console?",
+        ["console.log(\"Go Monkeys\”)", "console.hog(\"Go Huskies\")", "console.log(\"Lincoln\")", "consoul.log(\"Go Huskies\")"]
+    ],
+    [
+        "Which of the following will yield a pop-up message requiring user input?",
+        ["alert()", "pop()", "message()", "prom?()"]
+    ],
+    [
+        "How do you round numbers to the nearest integer?",
+        ["round()", "Math.square()", "Using fingers", "Consult an Asian"]
+    ]
+]);
+
+var timer = document.getElementById("timer");
+var question = "";
+var questionNumber = 1;
+var answers = [];
+var secondsLeft;
+var timerInterval;
+var highScores = [];
+var totalQuestions = quizData.size;
+let score = 0;
+
+for (let i = 0; i < 4; i++) {
+    document
+      .getElementById("button" + (i + 1))
+      .addEventListener("click", function () {
+        check(this);
+      });
+}
+
+function setTimer (time) {
+    secondsLeft = time;
+    timer.textContent = secondsLeft;
+    timerInterval = setInterval(function () {
+        secondsLeft--;
+        timer.textContent = secondsLeft;
+        if (secondsLeft = 0) {
+            load();
+        }
+    }, 1000);
+}
+
+function check (selection) {
+    // impossible to answer correctly, but this unnecessary code can be reused to not-impossible quizzes
+    if (selection.textContent === answers[4]) {
+        console.log(selection);
+        score++;
+        load();
+        ;
+    }   else {
+        setTimeout(function() { alert("WRONG!"); }, 2)
+        load();
+    }
+}
+
+function load() {
+    if (questionNumber > totalQuestions) {
+        end();
+    }   else {
+        question = Array.from(quizData.keys())[questionNumber - 1];
+        document.getElementById("question").textContent = question;  
+        answers = quizData.get(question);
+        for (let i = 0; i < 4; i++) {
+            document.getElementById("button" + (i + 1)).textContent = answers[i];
+        }
+        questionNumber++;
+    }
+}
+
+function end() {
+    document.getElementById("buttons").remove();
+    clearInterval(timerInterval);
+    document.getElementById("question").textContent =
+    "Your score:" + score;
+// Figuring out how to save score
+
+
+}
+
+
+load();
